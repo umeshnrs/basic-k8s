1ff1ee678352c555013599257db88225901aee57
umeshnagarro <umesh.gupta@nagarro.com>
2023-02-03 21:00:14 +0530

    main add

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# project-ui
-ng new
+```sh
+3. npm install
+4. npm start
+```
+
+The application is running on [localhost](http://localhost:3000).
---
+++ app.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 3005);
+app.set('views', __dirname + '/app/server/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/app/public'));
+require('./app/routes')(app);
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Listen in ' + app.get('port') + ' app....');
+});
